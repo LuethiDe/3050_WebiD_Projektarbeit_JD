9baa0eeec4fcb1b2770b0c73a3e63ab00a172d9f
Denis Lüthi <denis.luethi@students.fhnw.ch>
2025-12-07 18:26:36 +0100

    update Frontend

--- App/src/Header.jsx
+++ App/src/Header.jsx
@@ -1,1 +1,27 @@
-export const Header = () => <div className="header">-- Projekttitel --</div>;
+export const Header = () => (
+  <div className="header">
+    <h1
+      id="title"
+      style={{
+        marginLeft: "35px",
+      }}
+    >
+      Projektarbeit
+    </h1>
+    <div
+      style={{
+        flexGrow: "2",
+      }}
+    ></div>
+    <button className="button buttonHover"> ---Test---</button>
+    <button
+      className="button buttonHover"
+      style={{
+        marginRight: "35px",
+      }}
+    >
+      {" "}
+      ---Test---
+    </button>
+  </div>
+);
--- App/src/Map.jsx
+++ App/src/Map.jsx
@@ -2,15 +2,21 @@ import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
 
 export const Map = () => {
   return (
-    <MapContainer
-      center={[47.5348, 7.6419]}
-      zoom={20}
-      style={{ height: "100vh", width: "100%" }}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        //attribution="&copy; OpenStreetMap contributors"
-      />
-    </MapContainer>
+    <div>
+      <MapContainer
+        center={[47.5348, 7.6419]}
+        zoom={20}
+        style={{ height: "50vh", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          //attribution="&copy; OpenStreetMap contributors"
+        />
+      </MapContainer>
+      <div className="aside">
+        <h3 className="text">Informationen</h3>
+        <p className="text">Bla Bla Bla.</p>
+      </div>
+    </div>
   );
 };
